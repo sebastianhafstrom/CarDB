76e0df67e03fc9d805531c697715574e7f5452d6
Sebastian Hafström <sebastian.hafstrom@telnesstech.com>
2024-12-06 23:32:25 +0100

    set cors method to *

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ require("dotenv").config();
 
 const coreConfig = {
   origin: [process.env.FRONTEND_URL],
-  methods: ["GET", "OPTIONS"],
+  methods: ["*"],
   allowedHeaders: ["x-api-key"],
 };
 
