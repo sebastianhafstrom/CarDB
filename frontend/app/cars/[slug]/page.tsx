6678fd6926eb407759b1a5b4607a35bbda021fa1
Sebastian Hafström <sebastian.hafstrom@telnesstech.com>
2024-12-21 12:45:10 +0100

    cosmetics

--- frontend/app/cars/[slug]/page.tsx
+++ frontend/app/cars/[slug]/page.tsx
@@ -24,71 +24,74 @@ export default function CarPage() {
   if (car) {
     return (
       <>
-        <div className="flex justify-between items-center">
-          <h1 className="text-4xl font-bold pb-4">
-            {car.brand.name + " " + car.name}
-          </h1>
-          <div>Body: {car.bodyType}</div>
-          <div>
-            Brand:{" "}
-            <Link href={`/brands/${car.brand.slug}`}>{car.brand.name}</Link>
+        <div className="flex justify-between gap-4">
+          <div className="w-1/4 flex flex-col gap-4 items-start">
+            <h1 className="text-4xl font-bold pb-4">
+              {car.brand.name + " " + car.name}
+            </h1>
+            <div>Body: {car.bodyType}</div>
+            <div>
+              Brand:{" "}
+              <Link href={`/brands/${car.brand.slug}`}>{car.brand.name}</Link>
+            </div>
+            <Button asChild>
+              <Link href={car.website}>
+                {car.brand.name + " " + car.name} på{" "}
+                {new URL(car.website).hostname.replace(/^www\./, "")}
+              </Link>
+            </Button>
+
+            <div className="mb-4">
+              <h2 className="text-2xl font-bold">Dimensions</h2>
+              <div>Length: {car.length} mm</div>
+              <div>Width: {car.width} mm</div>
+              <div>Height: {car.height} mm</div>
+              <div>Wheelbase: {car.wheelbase} mm</div>
+            </div>
+          </div>
+          <div className="w-3/4">
+            <AspectRatio ratio={16 / 9}>
+              <img
+                src={car.imageUrl}
+                alt="Image"
+                className="rounded-md object-cover"
+              />
+            </AspectRatio>
           </div>
         </div>
-        <div className="w-3/4">
-          <AspectRatio ratio={16 / 9}>
-            <img
-              src={car.imageUrl}
-              alt="Image"
-              className="rounded-md object-cover"
-            />
-          </AspectRatio>
-        </div>
-        <div>
-          <Button asChild>
-            <Link href={car.website}>
-              Läs mer om {car.brand.name + " " + car.name} på{" "}
-              {new URL(car.website).hostname.replace(/^www\./, "")}
-            </Link>
-          </Button>
-        </div>
 
-        <div className="mb-4">
-          <h2>Variants</h2>
-          <ul>
-            {car.variants.map((variant) => (
-              <li className="mb-2" key={variant.id}>
-                <h3>{variant.name}</h3>
-                <ul>
-                  {variant.engines.map((engine) => (
-                    <li key={engine.id}>
-                      {engine.name} - {engine.fuelType} - {engine.power} hp
-                    </li>
-                  ))}
-                </ul>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="mb-4">
-          <h2>Professional reviews</h2>
-          <ul>
-            {car.professionalReviews.map((review) => (
-              <li key={review.id}>
-                <a href={review.website} target="_blank" rel="noreferrer">
-                  <h3>
-                    {review.title} (Rating: {review.rating})
-                  </h3>
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="mb-4">
-          <h2>Dimensions</h2>
-          <div>Length: {car.length} mm</div>
-          <div>Width: {car.width} mm</div>
-          <div>Height: {car.height} mm</div>
-          <div>Wheelbase: {car.wheelbase} mm</div>
+        <div className="flex">
+          <div className="mb-4 w-1/2">
+            <h2 className="text-2xl font-bold">Variants</h2>
+            <ul>
+              {car.variants.map((variant) => (
+                <li className="mb-2" key={variant.id}>
+                  <h3 className="text-xl">{variant.name}</h3>
+                  <ul>
+                    {variant.engines.map((engine) => (
+                      <li key={engine.id}>
+                        {engine.name} - {engine.fuelType} - {engine.power} hp
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="mb-4 w-1/2">
+            <h2 className="text-2xl font-bold">Professional reviews</h2>
+            <ul>
+              {car.professionalReviews.map((review) => (
+                <li key={review.id}>
+                  <a href={review.website} target="_blank" rel="noreferrer">
+                    <h3>
+                      {review.title} (Rating: {review.rating})
+                    </h3>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </>
     );
